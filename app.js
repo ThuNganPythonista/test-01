dc8ffe53fe8c59d36dab7f169e012fbb79e84a52
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-08-02 00:33:14 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Dưx liệu giả lập để test api postman
+let todos = [
+  { id: 1, task: "Buy groceries" },
+  { id: 2, task: "Clean the house" },
+];
+
+// parse request body ra
+app.use(express.json());
+
+// GET /todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+// POST /todos
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: todos.length + 1,
+    task: req.body.task,
+  };
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+// PUT /todos/:id
+app.put("/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const updatedTodo = req.body;
+  todos = todos.map((todo) =>
+    todo.id === todoId ? { ...todo, ...updatedTodo } : todo
+  );
+  res.json(updatedTodo);
+});
+
+// DELETE /todos/:id
+app.delete("/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  todos = todos.filter((todo) => todo.id !== todoId);
+  res.json({ message: "To-do item deleted successfully" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
